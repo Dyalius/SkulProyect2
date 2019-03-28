80b0b4f1d34786185308bfc26f33b5ac142a8933
Julio Cesar Torres Cama <julio.torres.c@tecsup.edu.pe>
2019-03-28 16:03:30 -0500

    Domain-Specific Classes Added to Models

---
+++ SkulProyect2/Models/Alumno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SkulProyect2.Models
+{
+    public class Alumno
+    {
+        public int Ciescolar { get; set; }
+        [ForeignKey("Representante")]
+        public int Cedularepresentante { get; set; }
+        public Representante Representante { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public int Telefono { get; set; }
+        public int Sexo { get; set; }
+    }
+}
---
+++ SkulProyect2/Models/Representante.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SkulProyect2.Models
+{
+    public class Representante
+    {
+        public int Cedula { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Parentesco { get; set; }
+        public int Telefono { get; set; }
+        public ICollection<Alumno> Alumnos { get; set; }
+    }
+}
